6675b8fd141d592e5aa1b2d8d174b6ef0bb39ef7
sbcarlos <>
2024-10-06 06:59:20 +0000

    Updates

---
+++ _next/static/ow7rrPNuGmMucTvPl9duJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/9689bc64329f19b0.css","static/chunks/pages/index-4e5f793d3bcdfa57.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
