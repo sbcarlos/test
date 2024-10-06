c453fa4a56a29ba18b8eb37c131bcd848f488bfe
sbcarlos <carlos@phiwebstudio.com>
2024-10-05 23:46:38 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  basePath: '', // or '/your-base-path' if you need one
+  // other configuration options...
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
